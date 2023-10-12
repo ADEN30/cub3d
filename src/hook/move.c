e29c338e6660bdea3abca7aabee9e2a1e5d227a5
Adrien Gallet <adrien.gallet07@gmail.com>
2023-10-12 09:08:33 +0200

    first commit

---
+++ src/hook/move.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eberger <eberger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 09:42:53 by eberger           #+#    #+#             */
+/*   Updated: 2023/08/06 14:00:27 by eberger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	move(t_vars *vars, int direction)
+{
+	t_pers *pers = vars->pers;
+	double	angle_rad;
+	int		test[2];
+	
+	angle_rad = pers->orientation * M_PI / 180.0;
+	if (direction == 1)
+		angle_rad = (pers->orientation + 180) * M_PI / 180.0;
+	else if (direction == 2)
+		angle_rad = (pers->orientation + 90) * M_PI / 180.0;
+	else if (direction == 3)
+		angle_rad = (pers->orientation + 270) * M_PI / 180.0;
+	double dx = (pers->ray_length * 2) * cos(angle_rad);
+	double dy = (pers->ray_length * 2) * sin(angle_rad);
+
+	int steps = fabs(dx) > fabs(dy) ? fabs(dx) : fabs(dy);
+
+	double xIncrement = dx / steps;
+	double yIncrement = dy / steps;
+
+	double x1 = pers->x;
+	double y1 = pers->y;
+	test[0] = xIncrement < 0 ?
+		floor(xIncrement) - 1 + pers->x:
+		ceil(xIncrement) + 1 + pers->x;
+	test[1] = yIncrement < 0 ?
+		floor(yIncrement) - 1 + pers->y:
+		ceil(yIncrement) + 1 + pers->y;
+	if (find_case(vars->map->lines, test[0] / DIMENSION, test[1] / DIMENSION) != '1')
+	{
+		x1 += xIncrement * 100 * vars->mlx->delta_time;
+		y1 += yIncrement * 100 * vars->mlx->delta_time;
+		pers->x = round(x1);
+		pers->y = round(y1);
+	}
+}
+
+void	clear_pixel(t_ray **ray)
+{
+	ray[0]->img->instances[ray[0]->img->count - 1].enabled = 0;
+}
+
+void	ft_hook_move(void* param)
+{
+	t_vars *vars;
+
+	vars = param;
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(vars->mlx);
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_UP) || mlx_is_key_down(vars->mlx, MLX_KEY_W))
+	{
+		mlx_delete_image(vars->mlx, vars->pers->rays[0]->img);
+		vars->pers->rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+		vars->pers->y = vars->pers->y - 10;
+		ft_raycasting(vars);
+	}
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_DOWN) || mlx_is_key_down(vars->mlx, MLX_KEY_S))
+	{
+		mlx_delete_image(vars->mlx, vars->pers->rays[0]->img);
+		vars->pers->rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+		vars->pers->y = vars->pers->y + 10;
+		ft_raycasting(vars);
+	}
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_A))
+	{
+		mlx_delete_image(vars->mlx, vars->pers->rays[0]->img);
+		vars->pers->rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+		vars->pers->x = vars->pers->x - 10;
+		ft_raycasting(vars);
+	}
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_D))
+	{
+		mlx_delete_image(vars->mlx, vars->pers->rays[0]->img);
+		vars->pers->rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+		vars->pers->x = vars->pers->x + 10;
+		ft_raycasting(vars);
+	}
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_LEFT))
+	{
+		mlx_delete_image(vars->mlx, vars->pers->rays[0]->img);
+		vars->pers->rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+		change_plan(vars);
+	}
+	if (mlx_is_key_down(vars->mlx, MLX_KEY_RIGHT))
+		turn_camera(vars);	
+}
---
+++ src/utils/init_struct.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_struct.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eberger <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 14:51:21 by eberger           #+#    #+#             */
+/*   Updated: 2023/08/06 16:09:42 by eberger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+t_map	*init_map(void)
+{
+	t_map	*map;
+
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (NULL);
+	map->lines = NULL;
+	return (map);
+}
+
+t_images	*init_images(void)
+{
+	t_images	*images;
+
+	images = malloc(sizeof(t_images));
+	if (!images)
+		return (NULL);
+	images->count_north = 0;
+	images->north = NULL;
+	images->count_south = 0;
+	images->south = NULL;
+	images->count_west = 0;
+	images->west = NULL;
+	images->count_east = 0;
+	images->east = NULL;
+	images->floor_image = NULL;
+	images->roof_image = NULL;
+	images->wall_image = NULL;
+	return (images);
+}
+
+t_style	*init_style(void)
+{
+	t_style	*style;
+
+	style = malloc(sizeof(t_style));
+	if (!style)
+		return (NULL);
+	style->images = init_images();
+	if (!style->images)
+		return (free(style), NULL);
+	style->north_path = NULL;
+	style->south_path = NULL;
+	style->west_path = NULL;
+	style->east_path = NULL;
+	style->define_floor = 0;
+	style->floor = 0;
+	style->define_roof = 0;
+	style->roof = 0;
+	if (MAP == 1)
+		style->wall = get_rgba(0, 0, 0, 255);
+	else
+		style->wall = get_rgba(0, 0, 0, 0);
+	return (style);
+}
+
+t_ray	**init_ray(t_vars *vars)
+{
+	t_ray	**rays;
+
+	rays = malloc(sizeof(t_ray *) * (1));
+	if (!rays)
+		return (NULL);
+	rays[0] = malloc(sizeof(t_ray) * (1));
+	rays[0]->img = mlx_new_image(vars->mlx, vars->mlx->width, vars->mlx->height);
+	rays[0]->count_points = 0;
+	rays[0]->angle = 0.00;
+	rays[0]->coefdir = 0.00;
+	rays[0]->dir = 0.00;
+	rays[0]->d1 = 0.00;
+	rays[0]->d2 = 0.00;
+	rays[0]->d3 = 0.00;
+	return (rays);
+}
+
+t_pers	*init_pers(int x, int y, char c, t_vars *vars)
+{
+	t_pers	*pers;
+
+	pers = malloc(sizeof(t_pers));
+	if (!pers)
+		return (NULL);
+	pers->x = 0;
+	pers->y = 0;
+	if (x != -1)
+		pers->x = x;
+	if (y != -1)
+		pers->y = y;
+	pers->rays = init_ray(vars);
+	pers->ray_length = ft_lstsize(vars->map->lines) > largest_line(vars->map->lines) ?
+		ft_lstsize(vars->map->lines) * DIMENSION:
+		largest_line(vars->map->lines) * DIMENSION;
+	if (c == 'S')
+		pers->orientation = 90;
+	else if (c == 'N')
+		pers->orientation = 270;
+	else if (c == 'W')
+		pers->orientation = 0;
+	else if (c == 'E')
+		pers->orientation = 180;
+	return (pers);
+}
+
+t_vars	*init_vars(void)
+{
+	t_vars	*vars;
+
+	vars = malloc(sizeof(t_vars));
+	if (!vars)
+		return (NULL);
+	vars->map = NULL;
+	vars->pers = NULL;
+	vars->style = NULL;
+	return (vars);
+}
