c82b1b94d53993ea5cd5e8434c4bd70bbd0911bc
jmathieu <jmathieu@student.42mulhouse.fr>
2023-12-26 18:19:58 +0100

    new try on checking wall

--- src/raycasting/ray2.c
+++ src/raycasting/ray2.c
@@ -3,49 +3,38 @@
 void vertical_intersection(t_vars *vars, double *x, double *y)
 {
     double  tangent;
-    double  x_step;
-    double  y_step;
     bool    ray_right;
 
-    tangent = tan(vars->pers->angle);
-    if (vars->pers->angle > M_PI / 2 && vars->pers->angle < 3 * M_PI / 2)
+    tangent = tan(vars->pers->angle * M_PI / 180);
+    if (vars->pers->angle * M_PI / 180 > M_PI / 2 && (vars->pers->angle * M_PI / 180) < 3 * M_PI / 2)
         ray_right = false;
     else
         ray_right = true;
+    //DIMENSION a changer
     if (ray_right)
-        *x = *x + 1.0;
-    *y = vars->pers->y + (*x - vars->pers->x) * tangent;
-    if (ray_right)
-        x_step = 1.0;
+        *x = floor(*x / DIMENSION) * DIMENSION + DIMENSION;
     else
-        x_step = -1.0;
-    y_step = x_step * tangent;
-    *x += x_step;
-    *y += y_step;
+        *x = floor(*x / DIMENSION) * DIMENSION - 1;
+    *y = vars->pers->y + (*x - vars->pers->x) * tangent;
 }
 
 void horizontal_intersection(t_vars *vars, double *x, double *y)
 {
     double  tangent;
-    double  _step;
-    double  y_step;
     bool    ray_up;
     
-    tangent = tan(vars->pers->angle);
-    if (vars->pers->angle > 0 && vars->pers->angle < M_PI)
+
+    tangent = tan(vars->pers->angle * M_PI / 180);
+    if (vars->pers->angle * M_PI / 180 > 0 && (vars->pers->angle * M_PI / 180) < M_PI)
         ray_up = true;
     else
         ray_up = false;
-    if (ray_up == false)
-        *y = *y + 1.0;
-    *x = vars->pers->x + (*y - vars->pers->y) / tangent;
     if (ray_up)
-        y_step = -1.0;
+        *y = floor(*y / DIMENSION) * DIMENSION - 1;
     else
-        y_step = 1.0;
-    x_step = y_step / tangent;
-    *x += x_step;
-    *y += y_step;
+        *y = floor(*y / DIMENSION) * DIMENSION + DIMENSION;
+    if (tangent != 0)
+        *x = vars->pers->x + (*y - vars->pers->y) / tangent;
 }
 
 double dist(t_vars *vars, double x, double y)
@@ -66,12 +55,14 @@ void    check_lowest_move(t_vars *vars, double *x, double *y)
 
 	h[0] = v[0] = *x;
     h[1] = v[1] = *y;
+    // Revoir le calcul du pas pour le prochain pixel : touver un truc coherent
     horizontal_intersection(vars, &h[0], &h[1]);
     dist_h = dist(vars, h[0], h[1]);
     vertical_intersection(vars, &v[0], &v[1]);
     dist_v = dist(vars, v[0], v[1]);
-        //printf("xv = %f yv = %f\n", v[0], v[1]);
-        //printf("xh = %f yh = %f\n", h[0], h[1]);
+    printf("xv= %f yv= %f\n", v[0], v[1]);
+    printf("xh= %f yh= %f\n", h[0], h[1]);
+    //probleme de choix en fonction de l'angle
     if (dist_h < dist_v)
     {
         *x = h[0];
@@ -102,19 +93,16 @@ void	start_plan(t_vars* vars)
 	i = 599;
 	mlx_image_to_window(vars->mlx, vars->pers->rays[0]->img, 0, 0);
 	vars->pers->rays[0]->points = malloc(sizeof(t_point) * 600);
-    //printf("Angle actuel: %f\n", vars->pers->angle);
-	while (vars->pers->angle - (FOV / 2) < angles + (FOV / 2))
+	while (vars->pers->angle < angles + (FOV / 2))
 	{
+        //printf("Angle actuel: %f\n", vars->pers->angle);
         y = vars->pers->y;
         x = vars->pers->x;
-        int count = 0;
 		while (!test_wall2(vars, x, y) && x > 0 && x < vars->mlx->width && y > 0 && y < vars->mlx->height)
 		{ 
 			mlx_put_pixel(vars->pers->rays[0]->img, round(x), round(y), get_rgba(255, 0, 0, 255));
             check_lowest_move(vars, &x, &y);
             printf("x = %f | y = %f\n", x, y);
-            printf("count = %d\n", count++);
-            sleep(1);
 		}
 		vars->pers->rays[0]->points[i].y = y;
 		vars->pers->rays[0]->points[i].x = x;
